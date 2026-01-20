6c8ebfff1494aa9b78f5be65234f6be89d00e90d
KMV531 <koladjamomovinnybrayan3@gmail.com>
2026-01-20 14:46:19 +0100

    Gallery Photo section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { AboutSection } from "@/sections/AboutSection";
 import CulturalHighlights from "@/sections/culturalHighlights";
 import DebunkingMyths from "@/sections/DebunkingMyths";
+import GaleyPhoto from "@/sections/GaleryPhoto";
 import HeroSection from "@/sections/HeroSection";
 
 function HomePage() {
@@ -10,6 +11,7 @@ function HomePage() {
       <AboutSection />
       <CulturalHighlights />
       <DebunkingMyths />
+      <GaleyPhoto />
     </main>
   );
 }
